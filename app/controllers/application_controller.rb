92df0efcc0d80040e09ac93a56ae159d37582545
Ellis Grouse <egrouse@ninjad.co>
2011-01-31 22:45:47 +0000

    added view link page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_site_path
+  
   protect_from_forgery
+  
+  def set_site_path
+    @sitepath = 'http://' + request.env['HTTP_HOST'] + '/'
+  end
 end
